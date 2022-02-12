4370719e653c35e35dc2618b5e26c54cbbcf58bc
Márcio Rogério Sieto <mmmsieto@hotmail.com>
2022-02-12 20:13:54 -0300

    first commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,16 @@
+import * as B from './Button.styles'
+import {ButtonHTMLAttributes, DetailedHTMLProps} from "react";
+
+export interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+    variant: 'danger' | 'text' | 'primary'
+    label: string
+}
+
+export default function Button({label, variant, ref, ...props}: ButtonProps) {
+
+    return <B.Wrapper variant={variant} {...props} >
+
+        { label }
+
+    </B.Wrapper>
+}
